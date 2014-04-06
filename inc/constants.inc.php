a8fb4b7ac96e0c965ecc91bc2ee6d4d0b8697320
archugs <archana.gurusamy@gmail.com>
2014-04-06 21:19:26 +0530

    Modified database configuration settings to connect to ClearDB

--- inc/constants.inc.php
+++ inc/constants.inc.php
@@ -1,9 +1,9 @@
 <?php
-
+	$url = parse_url(getenv(CLEARDB_DATABASE_URL));
 	//Database credentials
-	define('DB_HOST', 'localhost');
-	define('DB_USER', 'root');
-	define('DB_PASS', '');
-	define('DB_NAME', 'qp_db');
+	define('DB_HOST', $url["host"]);
+	define('DB_USER', $url["user"]);
+	define('DB_PASS', $url["pass"]);
+	define('DB_NAME', (substr($url["path"], 1)));
 	
 ?>
