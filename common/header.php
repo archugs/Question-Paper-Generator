af488b4cb5a0f6fa4caf60a6c49be0e459ded674
archugs <archana.gurusamy@gmail.com>
2014-07-29 23:31:03 +0530

    Redesigned login page

--- common/header.php
+++ common/header.php
@@ -14,6 +14,7 @@
 	<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
 	<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
 	<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css">
+	<link rel="stylesheet" href="font-awesome-4.1.0/css/font-awesome.min.css">
 </head>
 
 <body>
--- login.php
+++ login.php
@@ -35,18 +35,22 @@
 	else:
 ?>
 
-	<h4>Please login to continue</h4>
-	<br />
-	<form method="post" action="login.php" name="loginform" id="loginform">
-		<div>
-			<label for="userid">UserID</label>
-			<input type="text" name="userid" id="userid" />
-			<br /><br />
-			<label for="password">Password</label>
-			<input type="password" name="password" id="password" />
-			<br /><br />
-			<input type="submit" name="login" id="login" value="Login" class="button" />
-		</div>
+	<form method="post" action="login.php" name="login-form" id="login-form">
+	<h1>Log In</h1>
+	<fieldset id="inputs">
+		<p class="field">
+			<input type="text" name="userid" id="userid" placeholder="Username" autofocus required />
+			<i class="fa fa-user fa-lg"></i>
+		</p>
+		<p class="field">
+			<input type="password" name="password" id="password" placeholder="Password" required />
+			<i class="fa fa-lock fa-lg"></i>
+		</p>
+	</fieldset>
+	<fieldset id="actions">
+			<input type="submit" name="login" id="login" value="Log in" />
+			<a href="">Forgot password?</a><a href="">New user?</a>
+	</fieldset>
 	</form><br /><br />
 
 <?php
